57b245d58ba6b911fc652f1569f3a17da35664f6
jonrene <jrjonathanrene@gmail.com>
2021-12-11 11:35:06 -0500

    Add Phrase to Display
    Completed the addPhraseToDisplay method of the Phrase class.

--- js/Phrase.js
+++ js/Phrase.js
@@ -7,12 +7,35 @@ class Phrase{
         this.phrase = phrase.toLowerCase();
     }
 
+    // This function adds the hidden phrase to screen.
     addPhraseToDisplay(){
+        let phraseList = document.getElementById("phraseList");
+        
+        // This iterates through current phrase and creates an LI element for each character in phrase.
+        // Checks to see if each character is a space or character and assigns it the appropriate class.
+        // Then it sets each LI element's innerHTML to current character in phrase
+        for(let i = 0; i < this.phrase.length; i++){
+            // creates a list item to be added to the phrase list
+            let currChar = document.createElement("LI");
+            
+            // checks to see if list item should be a space or a letter and assigns it the appropriate class
+            if(this.phrase[i] === " "){
+                currChar.className = "space";
+            } else{
+                currChar.className = "letter";
+            }
 
+            // sets innerHTML of list item to current character in phrase
+            currChar.innerHTML = this.phrase[i];
+
+            // adds phrase to screen. 
+            phraseList.appendChild(currChar);
+        }
     }
 
     checkLetter(letter){
-        return this.phrase.includes(letter);
+        // checks to see if given letter is in the phrase
+        return this.phrase.includes(letter.toLowerCase());
     }
 
     showMatchedLetter(){
