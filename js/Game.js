ae46fa028471870e224f1256b57d468b85f2659d
jonrene <jrjonathanrene@gmail.com>
2021-12-11 12:26:27 -0500

    Game Constructor
    Completed the constructor method for the Game class

--- js/Game.js
+++ js/Game.js
@@ -4,7 +4,18 @@
 
 class Game{
     constructor(){
-        
+        // Keeps track of missed guesses player has
+        this.missed = 0;
+        // Stores phrases to be used for game
+        this.phrases = [new Phrase("Do not go gentle into that good night"), 
+                        new Phrase("Life is like a box of chocolates"), 
+                        new Phrase("Break a leg"), 
+                        new Phrase("Better late then never"),
+                        new Phrase("The best of both worlds")
+        ];
+
+        // current active phrase. 
+        activePhrase = null;
     }
 
 }
